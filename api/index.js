6dbeee9eae4b36546de32f00a0138d85747f8217
dmh1001 <dmh1001@alu.ubu.es>
2022-06-02 20:27:21 +0200

    Phonebook backend step3

--- api/index.js
+++ api/index.js
@@ -31,11 +31,23 @@ app.get('/api/persons', (request, response) => {
     response.json(phonebook)
 })
 
+app.get('/api/persons/:id',(request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
+
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+
+    
+})
 
 app.get('/info',(request, response) => {
     
-    let phonebookSize = phonebook.length
-    let time = new Date().toUTCString()
+    const phonebookSize = phonebook.length
+    const time = new Date().toUTCString()
     response.send(`<div><p>Phonebook has info for ${phonebookSize} people<p/><p>${time}<p/><div/>`)
 
 })
