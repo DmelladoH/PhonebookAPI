eb12dc479d1cfcfaa4651521fb2fb25574439970
dmh1001 <dmh1001@alu.ubu.es>
2022-06-03 21:13:10 +0200

    Middleware

--- api/index.js
+++ api/index.js
@@ -1,92 +1,100 @@
-const http = require('http')
 const express = require('express')
 const app = express()
+
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method: ', request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ', request.body)
+  console.log('------')
+
+  next()
+}
+
+const unknownEndPoint = (request, response, next) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
 let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
 ]
 
+app.use(requestLogger)
 
-app.get('/info',(request, response) => {
-    
-    const phonebookSize = phonebook.length
-    const time = new Date().toUTCString()
-    response.send(`<div><p>Phonebook has info for ${phonebookSize} people<p/><p>${time}<p/><div/>`)
-
+app.get('/info', (request, response) => {
+  const phonebookSize = phonebook.length
+  const time = new Date().toUTCString()
+  response.send(`<div><p>Phonebook has info for ${phonebookSize} people<p/><p>${time}<p/><div/>`)
 })
-
 
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
+  response.json(phonebook)
 })
 
-app.get('/api/persons/:id',(request, response) => {
-    const id = Number(request.params.id)
-    person = phonebook.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = phonebook.find(person => person.id === id)
 
-    if(person){
-        response.json(person)
-    }else{
-        response.status(404).end()
-    }
-
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
-
 
 app.post('/api/persons', (request, response) => {
+  const body = request.body
 
-    const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'The name or number is missing'
+    })
+  }
 
+  if (phonebook.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'The name already exists in the phonebook'
+    })
+  }
 
-    if(!body.name || !body.number){
-        return response.status(400).json({
-            error: 'The name or number is missing'
-        })
-    }
+  const person = {
+    id: Math.floor(Math.random() * 1000),
+    name: body.name,
+    number: body.number
+  }
 
-    if(phonebook.find(person => person.name === body.name)){
-        return response.status(400).json({
-            error: 'The name already exists in the phonebook'
-        })
-    }
-
-    const person = {
-        id: Math.floor(Math.random() * 1000),
-        name: body.name, 
-        number: body.number
-    }
-
-    phonebook = phonebook.concat(person)
-    response.json(person)
-   
+  phonebook = phonebook.concat(person)
+  response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(person => person.id !== id)
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(person => person.id !== id)
 
-    response.status(204).end()
+  response.status(204).end()
 })
+
+app.use(unknownEndPoint)
 
 const PORT = 3001
 app.listen(PORT)
